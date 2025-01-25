affc02978682fe7eea2193a4d9dbbd87e06654f5
Pierre-Etienne Gallardo <pierre-etienne@unicorn-stc.de>
2025-01-25 11:20:15 +0200

    Spring project initialization

---
+++ src/main/java/com/betting/stockbetting/StockbettingApplication.java
@@ -0,0 +1,13 @@
+package com.betting.stockbetting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockbettingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockbettingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/betting/stockbetting/StockbettingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betting.stockbetting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockbettingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
